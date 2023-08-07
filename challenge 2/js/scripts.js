caf8dce803ab650e54e9df83d5dd7f7eada85c7d
Taylor <taylordavids997@gmail.com>
2023-08-07 15:41:40 +0200

    add lms code

---
+++ challenge 2/js/scripts.js
@@ -0,0 +1,16 @@
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+if (hourOfDay && minuteOfDay !== null && hourOfDay === 0 && minuteOfDay === 0) {
+	const taxAsDecimal = tax / 100
+    const startingAfterTax = salary * 1 - taxAsDecimal
+	const balance = startingAfterTax - transport - food - rent
+    console.log(balance)
+}
