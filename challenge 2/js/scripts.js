321f4e80f036092c82ff0f2df3e141f74bf49285
Taylor <taylordavids997@gmail.com>
2023-08-09 08:43:12 +0200

    add comments of changes and reasons

--- challenge 2/js/scripts.js
+++ challenge 2/js/scripts.js
@@ -8,9 +8,9 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-if ((hourOfDay !== undefined && minuteOfDay !== undefined) && (hourOfDay == '00') && (minuteOfDay == '00')) {
-	const taxAsDecimal = (salary * parseInt(tax)) / 100         
+if ((hourOfDay !== undefined && minuteOfDay !== undefined) && (hourOfDay == '00') && (minuteOfDay == '00')) {   // hourOfDay/minuteOfDay opposit of defined
+	const taxAsDecimal = (salary * parseInt(tax)) / 100                 // salary * tax / 100
     const startingAfterTax = salary * 1 - taxAsDecimal
 	const balance = startingAfterTax - rent - food - transport 
-    console.log('R', balance.toFixed(2))
+    console.log('R', balance.toFixed(2))                                // R string toFixed number with  2 decimal
 }
